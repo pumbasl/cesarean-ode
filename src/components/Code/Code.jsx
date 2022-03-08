98056fdb9793adcef653f53ea0c7e687ffaa8f69
Denis Masson <pumbasl60@gmail.com>
2022-03-08 12:47:06 +0200

    Базовая логика и верстка

--- src/components/Code/Code.jsx
+++ src/components/Code/Code.jsx
@@ -2,47 +2,141 @@
 import React, { useState } from 'react';
 import { jsx, css } from '@emotion/react'
 
-const styled = css`
-    margin-top: 10px;
-    background-color: rgb(236, 236, 236);
-    border-radius: 0 0 3px 3px;
-    color: black;
-    padding: 10px;
-    text-align: center;
-`;
+import { codeEncrypt, codeDecrypt } from '../../service/codeEncryptAndDecrypt';
 
-const styledTextArea = css`
-    width: 50%;
-    height: 100px;
-`;
+const styled = {
+    component: css`
+        margin-top: 10px;
+        background-color: rgb(236, 236, 236);
+        border-radius: 0 0 3px 3px;
+        color: black;
+        padding: 10px;
+        text-align: center;
+    `,
+    TextArea: css`
+        width: 100%;
+        height: 100px;
+        resize: vertical;
+    `,
+
+    ErrorComponent: css`
+        margin-bottom: 8px;
+        background-color: #261c2c;
+        color: white;
+        border-radius: 3px;
+        padding: 10px;
+    `,
+
+    labels: css`
+        margin-bottom: 5px;
+        label {
+            margin-right: 5px;
+        }
+    `,
+
+    resultTextArea: css`
+        width: 99%;
+        height: 100px;
+        resize: vertical;
+    `,
+
+    button: css`
+        padding: 10px 30px 10px 30px;
+        background-color: #A3423C;
+        color: white;
+        border: 0;
+        border-radius: 3px;
+
+        &:hover{
+            transition: all .3s ease-out;
+            background-color: #DE834D;
+        }
+    `
+};
 
 export default function Code(){
-    const [ encrypt, setEncrypt ] = useState('');
+    const [ text, setText ] = useState('');
     const [ type, setType ] = useState('');
+    const [ encryptStep, setEncryptStep ] = useState(3);
+    const [ result, setResult ] = useState();
+    const [ errors, setErrors ] = useState([]);
+
+    const ArrayOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        if(type === '' || encrypt === '') {
+            setErrors([
+                {
+                    message: 'Вы не заполнили поле сообщения или не выбрали тип обработки.'
+                }
+            ]);
+        } else {
+            if(type === 'encrypt'){
+                setResult(codeEncrypt(text, encryptStep));
+            }
+            if(type === 'decrypt'){
+                setResult(codeDecrypt(text, encryptStep));
+            }
+        }
     };
 
+    const ErrorComponent = () => (
+        errors.map((error, index) => (
+            <div key={index} css={styled.ErrorComponent}>
+                {error.message}
+            </div>
+        ))
+    );
+
     return(
-        <div css={styled}>
+        <div css={styled.component}>
             <form onSubmit={handleSubmit}>
-                <input type='radio' name="type" value="encrypt" onChange={(e => setType(e.target.value))} />
-                <label htmlFor="encrypt">Зашифровать</label>
+                
+                <div css={styled.labels}>
+                    <input type='radio' id="encrypt" name="type" value="encrypt" onChange={(e => setType(e.target.value))} />
+                    <label htmlFor="encrypt">Зашифровать</label>
 
-                <input type='radio' name="type" value="decrypt" onChange={(e => setType(e.target.value))} />
-                <label htmlFor="decrypt">Расшифровать</label>
+                    <input type='radio' id="decrypt" name="type" value="decrypt" onChange={(e => setType(e.target.value))} />
+                    <label htmlFor="decrypt">Расшифровать</label>
+                </div>
 
-                <br />
-                <textarea
-                    css={styledTextArea}
-                    value={encrypt}
-                    onChange={(e) => setEncrypt(e.target.value)}
-                    placeholder="Введите Ваше сообщение"
-                />
+                <div css={styled.labels}>
+                    <label htmlFor="selectSteps">Шаг сдвига:</label>
+                    <select id="selectSteps" value={encryptStep} onChange={(e) => setEncryptStep(Number(e.target.value))}>
+                        {ArrayOptions.map((number) => (
+                            <option value={number} key={number}>
+                                {number}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+
+                <div className='row'>
+                    <div className='col'>
+                        <textarea
+                            css={styled.TextArea}
+                            value={text}
+                            onChange={(e) => setText(e.target.value)}
+                            placeholder="Введите Ваше сообщение"
+                        />
+                    </div>
+
+                    <div className='col'>
+                        <textarea 
+                            css={styled.resultTextArea}
+                            value={result}
+                            readOnly
+                            placeholder="Тут будет Ваш результат"
+                        />
+                    </div>
+                </div>
 
                 <hr />
-                <button type="submit">Отправить</button>
+
+                { errors.length !== 0 ? (<ErrorComponent />) : (null) }
+                <button css={styled.button} type="submit">Отправить</button>
             </form>
         </div>
     );
--- src/service/codeEncryptAndDecrypt.js
+++ src/service/codeEncryptAndDecrypt.js
@@ -1,4 +1,114 @@
-const codeEncrypt = () => {};
-const codeDecrypt = () => {};
+const OtherSymbols = [' ',',','.',':',';','!','?','-','_','=','+','(',')','[',']','@','`',"'",'"','<','>','|','/','%','$','^','&','*','~'];
+const Numbers = ['0','1','2','3','4','5','6','7','8','9'];
+const RusAlfUp = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'];
+const RusAlfLower = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'];
+const EngAlfUp = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+const EngAlfLower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','m','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+let RusAlfUpEncrypt = Array(33);
+let RusAlfLowerEncrypt = Array(33);
+let EngAlfUpEncrypt = Array(26); 
+let EngAlfLowerEncrypt = Array(26);
+let NumbersEncrypt = Array(10);
+
+let pos;
+
+function contains(symb, arr) {
+    const letter = symb;
+    pos = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (letter === arr[i]) {
+        pos = i;
+        return true;
+      }
+    }
+}
+
+function CezarEncrypt(stap, arr) {
+    let CopyAlf = arr.slice();
+    let i = 0;
+    
+    while ((i + stap) < (CopyAlf.length)) {
+      const buff = CopyAlf[i];
+      CopyAlf[i] = CopyAlf[i + stap];
+      CopyAlf[i + stap] = buff;
+      i++;     
+    }
+    return CopyAlf;
+}
+
+function initEncrypt(UserStep) {
+    RusAlfUpEncrypt = CezarEncrypt(UserStep, RusAlfUp);
+    RusAlfLowerEncrypt = CezarEncrypt(UserStep, RusAlfLower);
+    NumbersEncrypt = CezarEncrypt(UserStep, Numbers);
+    EngAlfUpEncrypt = CezarEncrypt(UserStep, EngAlfUp);
+    EngAlfLowerEncrypt = CezarEncrypt(UserStep, EngAlfLower);
+}
+
+const codeEncrypt = (text, stap) => {
+    initEncrypt(stap);
+
+    let result = '';
+    for (let i = 0; i <= text.length; i++) {
+        let symbol = text[i];
+        if (contains(symbol, OtherSymbols)) {
+        result += symbol;
+        }
+        if (contains(symbol, Numbers)) {
+        symbol = NumbersEncrypt[pos];
+        result += symbol;
+        }
+        if (contains(symbol, RusAlfUp)) {
+            symbol = RusAlfUpEncrypt[pos];
+            result += symbol;
+        }
+        if ((contains(symbol, RusAlfLower))) {
+            symbol = RusAlfLowerEncrypt[pos];
+            result += symbol;
+        }
+        if (contains(symbol, EngAlfUp)) {
+            symbol = EngAlfUpEncrypt[pos];
+            result += symbol;
+        }
+        if ((contains(symbol, EngAlfLower))) {
+            symbol = EngAlfLowerEncrypt[pos];
+            result += symbol;
+        }
+    }
+    return result;
+};
+
+const codeDecrypt = (text, stap) => {
+    initEncrypt(stap);
+    
+    let result = '';
+    for (let i = 0; i <= text.length; i++) {
+        var symbol = text[i];
+        if (contains(symbol, OtherSymbols)) {
+            result += symbol;
+        }
+        if (contains(symbol, NumbersEncrypt)) {
+            symbol = Numbers[pos];
+            result += symbol;
+        }
+        if (contains(symbol, RusAlfUpEncrypt)) {
+            symbol = RusAlfUp[pos];
+            result += symbol;
+        }
+        if ((contains(symbol, RusAlfLowerEncrypt))) {
+            symbol = RusAlfLower[pos];
+            result += symbol;
+        }
+        if (contains(symbol, EngAlfUpEncrypt)) {
+            symbol = EngAlfUp[pos];
+            result += symbol;
+        }
+        if ((contains(symbol, EngAlfLowerEncrypt))) {
+            symbol = EngAlfLower[pos];
+            result += symbol;
+        }
+    }
+    return result;
+};
 
 export { codeEncrypt, codeDecrypt };
